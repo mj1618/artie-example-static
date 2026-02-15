990483f5e36d58f94905820c2293c148667b7d90
Matthew James <matt@MacBook-Pro-6.local>
2026-02-15 22:31:24 +0800

    react:

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 3000;
+const port = 3001;
 const staticDir = path.join(__dirname, 'src');
 
 // MIME types for common file extensions
